0ce49e1e777897e8ebababcb49f2f1a39c1c37d8
Pradeep <pradeepparit2001@gmail.com>
2026-04-25 14:28:51 +0530

    Removed restController

--- src/main/java/com/githubActions/githubActionsDemo/Controller/demoController.java
+++ src/main/java/com/githubActions/githubActionsDemo/Controller/demoController.java
@@ -1,14 +1,7 @@
 package com.githubActions.githubActionsDemo.Controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class demoController {
-    @GetMapping("/hello")
-    public ResponseEntity<String> demo() {
+    public void demo() {
         System.out.println("Hello World");
-        return ResponseEntity.ok("Hello World");
     }
 }
