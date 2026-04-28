4bc37fe4f19ffbae6f9699e17f2babe441f6c0dd
Pradeep <pradeepparit2001@gmail.com>
2026-04-28 14:30:33 +0530

    added controller API

--- src/main/java/com/githubActions/githubActionsDemo/Controller/demoController.java
+++ src/main/java/com/githubActions/githubActionsDemo/Controller/demoController.java
@@ -1,7 +1,15 @@
 package com.githubActions.githubActionsDemo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
 public class demoController {
-    public void demo() {
+    @GetMapping("/hello")
+    public String demo() {
         System.out.println("Hello World");
+        return "Hello World";
     }
 }
